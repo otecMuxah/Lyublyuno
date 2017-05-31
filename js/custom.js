1a257a6b82aacc72f713d328dd9a503fc8b27041
Mikhail Malyavin <mikhailmalyavin@MacBook-Pro-Mikhail.local>
2017-06-01 01:21:18 +0300

    select

--- js/custom.js
+++ js/custom.js
@@ -11,31 +11,15 @@ $(window).load(function(){
 		arrows: false,
 		fade: true
 	});
+	if($('.booking-form__date-input').length) {
+        $('.booking-form__date-input').datepicker();
+	}
 
-	$('.booking-form__date-input').datepicker();
 
-    $.widget( "custom.iconselectmenu", $.ui.selectmenu, {
-        _renderItem: function( ul, item ) {
-            var li = $( "<li>" ),
-                wrapper = $( "<div>", { text: item.label } );
+    if($('.booking-form__select').length) {
+        $('.booking-form__select').styler();
+    }
 
-            if ( item.disabled ) {
-                li.addClass( "ui-state-disabled" );
-            }
-
-            $( "<span>", {
-                style: item.element.attr( "data-style" ),
-                "class": "ui-icon " + item.element.attr( "data-class" )
-            })
-                .appendTo( wrapper );
-
-            return li.append( wrapper ).appendTo( ul );
-        }
-    });
-    $( ".booking-form__select" )
-        .iconselectmenu()
-        .iconselectmenu( "menuWidget" )
-        .addClass( "ui-menu-icons customicons" );
     $('.header__hamburger').on('click', function () {
 		$('.header__nav-holder').toggleClass('header__nav-holder_active')
 
